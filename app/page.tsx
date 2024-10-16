c83cb669a69c6cae593d33add3fc84ef95c330f3
Daniel Tomis <daniel.tomis@proton.me>
2024-10-16 20:41:37 +0200

    feat: add button link and change styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,53 @@
-'use client'
+'use client';
 import Nav from '@/components/Navbar';
 import CustomHeading from '@/components/CustomHeading';
 import GridBackground from '@/components/GridBackground';
 import TextContent from '@/components/TextContent';
-/* import Carousel from '@/components/Carousel';
- */
 import { Box } from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <>
-      <Box height="100vh" width="100vw">
+      {/* Header Section */}
+      <Box minHeight="100vh" width="100vw">
         <Nav />
         <GridBackground>
           <CustomHeading
-            heading='SARFAD'
-            description='CanSat tým'
+            heading="SARFAD"
+            description="CanSat tým"
           />
-        </GridBackground>
+        </GridBackground> 
       </Box>
-      <Box height='100vh' width='auto'>
+
+      {/* First TextContent Section */}
+      <Box minHeight="80vh" width="100%">
         <TextContent
           heading="Co je to CanSat?"
-          description='Soutěž pro týmy studentů, pořádaná Evropskou kosmickou agenturou. Cílem každého týmu je vytvořit 
-        funkční satelit o velikosti plechovky. Satelit je vypuštěn z rakety ve výšce jednoho kilometru.'
+          description="Soutěž pro týmy studentů, pořádaná Evropskou kosmickou agenturou. Cílem každého týmu je vytvořit 
+          funkční satelit o velikosti plechovky. Satelit je vypuštěn z rakety ve výšce jednoho kilometru."
           image={true}
           imageSrc="/cansat.svg"
           imageAlt="Rocket"
           button={true}
           buttonText="Zjistit více"
+          buttonLink="/"
+          />
+      </Box>
+
+      {/* Second TextContent Section */}
+      <Box minHeight="80vh" width="100%">
+        <TextContent
+          heading="Sleduj naší misi živě!"
+          description="Všechna data se během letu ukládají do databáze, odkud se poté zobrazují na našem webu v různých 
+          vizualizacích s aktualizováním v reálném čase."
+          image={true}
+          halfVisibleImage={false}
+          imageSrc="/live.svg"
+          imageAlt="Rocket"
+          button={true}
+          buttonText="Zobrazit web"
         />
       </Box>
-      {/* <Carousel
-        heading='Co umí náš CanSat'
-        images={[
-          { src: '/carousel1.svg', heading: 'Primární mise', description: 'Měření teploty a tlaku'},
-          { src: '/carousel2.svg', heading: 'Pozemní stanice', description: 'Přijímání dat z CanSatu'},
-          { src: '/carousel3.svg', heading: 'Konstruckce', description: 'Vysoká odolnost'},
-          { src: '/carousel4.svg', heading: 'Konstrukce', description: 'Měření teploty a tlaku'},
-          { src: '/carousel4.svg', heading: 'Konstrukce', description: 'Měření teploty a tlaku'},
-          { src: '/carousel4.svg', heading: 'Konstrukce', description: 'Měření teploty a tlaku'},
-          { src: '/carousel4.svg', heading: 'Konstrukce', description: 'Měření teploty a tlaku'},
-        ]}
-      /> */}
-      {/* <TextContent
-        heading="Sleduj naší misi živě!"
-        description='Všechna data se během letu ukládají do databáze, 
-        odkud se poté zobrazují na našem webu v různých vizualizacích s aktualizováním v reálném čase'
-        image={true}
-        imageSrc="/live.svg"
-        imageAlt="Rocket"
-        button={true}
-        buttonText="Zobrazit web"
-      /> */}
     </>
   );
 }
--- components/TextContent.tsx
+++ components/TextContent.tsx
@@ -11,35 +11,39 @@ interface TextContentProps {
 	heading: string;
 	description: string;
 	image: boolean;
+	halfVisibleImage?: boolean;
 	imageSrc: string;
 	imageAlt: string;
 	imageCaption?: string;
 	button: boolean;
 	buttonText: string;
+	buttonLink?: string;
 }
 
 const TextContent: React.FC<TextContentProps> = ({
 	heading,
 	description,
 	image,
+	halfVisibleImage,
 	imageSrc,
 	imageAlt,
 	imageCaption,
 	button,
 	buttonText,
+	buttonLink,
 }) => {
 	return (
 		<Box display="flex" alignItems="center" justifyContent="center" minHeight="100vh" width="100%">
 			<Grid
-				templateColumns={{ base: '1fr', sm: '2fr', md: '1.5fr 1fr' }}
-				gap={8}
+				templateColumns={{ base: '1fr', md: '1.2fr 1fr' }} // Adjust column width ratios
+				gap={{ base: 4, md: 8 }}  // Different gaps for mobile and desktop
 				alignItems="center"
-				justifyItems="center"
+				justifyItems={{ base: 'center', md: 'start' }}  // Center in mobile, align left in desktop
 				width="100%"
 				maxWidth="90vw"
 			>
-				<GridItem colSpan={1} width="80%">
-					<Box textAlign="left">
+				<GridItem colSpan={1} width={{ base: '100%', md: '80%' }}>
+					<Box textAlign={{ base: 'center', md: 'left' }}>
 						<Text fontSize={{ base: '4xl', md: '6xl' }} fontWeight="extrabold" mb={4}>
 							{heading}
 						</Text>
@@ -47,8 +51,10 @@ const TextContent: React.FC<TextContentProps> = ({
 							{description}
 						</Text>
 						{button && (
-							<Box display={{ base: 'flex', md: 'block' }} justifyContent="center">
+							<Box display="flex" justifyContent={{ base: 'center', md: 'start' }}>
 								<Button
+									as="a"
+									href={buttonLink}
 									bg="black"
 									color="white"
 									_hover={{ bg: '#301934' }}
@@ -63,7 +69,7 @@ const TextContent: React.FC<TextContentProps> = ({
 				</GridItem>
 
 				{image && (
-					<GridItem colSpan={1} width="100%" display="flex" justifyContent="center">
+					<GridItem colSpan={1} width={{ base: '90%', md: '100%' }} display="flex" justifyContent="center">
 						<ImageContent src={imageSrc} alt={imageAlt} caption={imageCaption} />
 					</GridItem>
 				)}
