c832bed26d47f2d536ca0054de5dc827d8f69c56
Daniel Tomis <danko.tomis@seznam.cz>
2025-04-28 00:28:53 +0200

    feat: add flask proxy for docker

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: true,
+	async rewrites() {
+	  return [
+		{
+		  source: '/sensor-data/:path*',
+		  destination: 'http://localhost:5000/sensor-data/:path*',
+		},
+	  ];
+	},
+};
 
-export default nextConfig;
+module.exports = nextConfig;
