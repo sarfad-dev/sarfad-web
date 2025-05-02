1dfb8ae7d42d07fef0a67a0170ca48cad5dc581f
tomisdaniel <daniel.tomis@proton.me>
2025-05-02 10:22:35 +0200

    feat: add members

--- client/app/clenove/page.tsx
+++ client/app/clenove/page.tsx
@@ -2,8 +2,8 @@
 import Nav from '@/components/Navbar';
 import CustomHeading from '@/components/CustomHeading';
 import GridBackground from '@/components/GridBackground';
-import TextContent from '@/components/TextContent';
 import { Box } from '@chakra-ui/react';
+import Carousel from '@/components/Carousel';
 
 export default function Members() {
   return (
@@ -14,9 +14,58 @@ export default function Members() {
         <Nav />
         <CustomHeading
           heading='Členové týmu'
-          description='WIP'
         />
       </Box>
+      <Carousel
+        id='team'
+        heading='Náš tým'
+        images={[
+          {
+            src: '/prokop.webp',
+            alt: 'Prokop Havlík',
+            captionHeading: 'Prokop Havlík',
+            captionDescription: 'Vedoucí týmu',
+            button: true,
+            overlayText: ''
+          },
+          {
+            src: '/daniel.webp',
+            alt: 'Daniel Tomis',
+            captionHeading: 'Daniel Tomis',
+            lightBg: true,
+            captionDescription: 'Programátor webu',
+            button: true,
+            overlayText: ''
+          },
+          {
+            src: '/jakub.webp',
+            alt: 'Jakub Holub',
+            captionHeading: 'Jakub Holub',
+            captionDescription: 'Manažer týmu',
+            button: true,
+            overlayText: ''
+          },
+          {
+            src: '/david.webp',
+            alt: 'David Oprštěný',
+            captionHeading: 'David Oprštěný',
+            captionDescription: 'PR a marketing',
+            button: true,
+            overlayText: ''
+          },
+          {
+            src: '/alexandr.webp',
+            alt: 'Alexandr Chovanec',
+            captionHeading: 'Alexandr Chovanec',
+            lightBg: true,
+            captionDescription: 'Konstrukce a výroba',
+            button: true,
+            overlayText: ''
+          },
+        ]}
+      >
+
+      </Carousel>
     </>
   );
 }
