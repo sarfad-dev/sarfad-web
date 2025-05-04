e75df893613d28d2b8806e5f73fa06c82445b636
Daniel Tomis <danko.tomis@seznam.cz>
2025-05-04 21:51:07 +0200

    feat: add heading

--- client/app/live-web/page.tsx
+++ client/app/live-web/page.tsx
@@ -3,23 +3,35 @@
 import Nav from '@/components/Navbar';
 import { Box, SimpleGrid } from '@chakra-ui/react';
 import ChartDashboard from '@/components/ChartDashboard';
+import CustomHeading from '@/components/CustomHeading';
+import GridBackground from '@/components/GridBackground';
 
 export default function LiveWeb() {
 	return (
-		<Box minHeight="100vh" width="100vw">
-			<Nav />
-			<Box pt="5rem" minHeight="80vh" width="100vw">
-				<Box gap={10} px={{ base: 4, md: 10 }}>
-					<SimpleGrid columns={{ base: 1, '2xl': 2 }} spacing={10}>
+		<>
+			<GridBackground>
+			</GridBackground>
+			<Box height="100vh" width="100vw">
+				<Nav />
+				<CustomHeading
+					heading='Live web'
+				/>
+			</Box>
+			<Box minHeight="100vh" width="100vw">
+				<Nav />
+				<Box pt="5rem" minHeight="80vh" width="100vw">
+					<Box gap={10} px={{ base: 4, md: 10 }}>
+						<SimpleGrid columns={{ base: 1, '2xl': 2 }} spacing={10}>
 							<ChartDashboard type="temperature" label="Temperature (°C)" color="tomato" />
 							<ChartDashboard type="humidity" label="Humidity (%)" color="royalblue" />
 							<ChartDashboard type="uvIndex" label="UV Index" color="orange" />
 							<ChartDashboard type="uvIndex" label="UV Index" color="purple" />
 							<ChartDashboard type="oxygen" label="Oxygen" color="green" />
 							<ChartDashboard type="carbonDioxide" label="Carbon Dioxide" color="pink" />
-					</SimpleGrid>
+						</SimpleGrid>
+					</Box>
 				</Box>
 			</Box>
-		</Box>
+		</>
 	);
 }
