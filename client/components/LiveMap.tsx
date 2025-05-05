d425bcd71f6e1dd40f4d88fe75db5a8e3cd7e69d
tomisdaniel <daniel.tomis@proton.me>
2025-05-05 17:23:53 +0200

    fix: not working livemap

--- client/components/LiveMap.tsx
+++ client/components/LiveMap.tsx
@@ -6,6 +6,17 @@ import 'leaflet/dist/leaflet.css';
 import { useEffect, useRef, useState } from 'react';
 import L from 'leaflet';
 
+import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+L.Icon.Default.mergeOptions({
+	iconRetinaUrl: markerIcon2x.src,
+	iconUrl: markerIcon.src,
+	shadowUrl: markerShadow.src,
+});
+
 const LiveMap = () => {
 	const [position, setPosition] = useState<[number, number]>([0, 0]);
 	const [path, setPath] = useState<[number, number][]>([]);
